ffde1a41e2cea89cee2cc810dbf165f868bcc547
Duarte Sousa <duartecacaosousa@gmail.com>
2024-01-31 14:59:29 +0000

    Play round function implementation

--- script.js
+++ script.js
@@ -16,54 +16,78 @@ function getComputerChoice(){
             /*alert('Rock');*/
             console.log('Rock');
             computerChoice = 'rock'
+            return computerChoice;
             break;
         case 1:
             /*alert('Paper');*/
             console.log('Paper');
             computerChoice = 'paper'
+            return computerChoice;
             break;
         case 2:
             /*alert('Scisors');*/
             console.log('Scisors');
             computerChoice = 'scisors'
+            return computerChoice;
             break;
     }
 
     
-}
+};
 
 
-function playerSelection(){
+function getplayerSelection(){
 
-    let playerSelection = prompt("Please choose between Rock, Paper or Scisors");
-    playerSelection = playerSelection.toLowerCase(); 
-    alert(playerSelection);
+    let getplayerSelection = prompt("Please choose between Rock, Paper or Scisors");
+    getplayerSelection = getplayerSelection.toLowerCase(); 
+    alert(getplayerSelection);
 
-    if (playerSelection === 'rock' | playerSelection === 'paper' | playerSelection === 'scisors') 
+    if (getplayerSelection === 'rock' | getplayerSelection === 'paper' | getplayerSelection === 'scisors') 
     {
-        alert('certo!!!!!');
+        /*alert('certo!!!!!');*/
+        return getplayerSelection;
     }
     else {   
         do {
-            playerSelection = prompt("Please choose ONLY between Rock, Paper or Scisors");
-            playerSelection = playerSelection.toLowerCase(); 
-        } while (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scisors'));
+            getplayerSelection = prompt("Please choose ONLY between Rock, Paper or Scisors");
+            getplayerSelection = getplayerSelection.toLowerCase(); 
+        } while (!(getplayerSelection === 'rock' || getplayerSelection === 'paper' || getplayerSelection === 'scisors'));
 
-        alert(playerSelection);
+        return getplayerSelection;
     }
+};
 
+
+const playerSelection = getplayerSelection();
+const computerSelection = getComputerChoice();
+let user_score = 0;
+let pc_score = 0;
+
+function playRound(playerSelection, computerSelection) {
+
+    alert("PC selection is: " + computerSelection + ". Player selection is: " + playerSelection)
+    if (playerSelection === computerSelection) {
+        alert('Its a tie!')
+    }
+    if (playerSelection === 'paper' & computerSelection === 'rock') {
+        alert('You win!!')
+    }
+    if (playerSelection === 'paper' & computerSelection === 'scisors') {
+        alert('You lose!!')
+    }
+    if (playerSelection === 'rock' & computerSelection === 'scisors') {
+        alert('You win!!')
+    }
+    if (playerSelection === 'rock' & computerSelection === 'paper') {
+        alert('You lose!!')
+    }
+    if (playerSelection === 'scisors' & computerSelection === 'paper') {
+        alert('You win!!')
+    }
+    if (playerSelection === 'scisors' & computerSelection === 'rock') {
+        alert('You lose!!')
+    }
+};
    
 
-
-    
-}
-playerSelection()
-
-
-/*function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));*/
+console.log(playRound(playerSelection, computerSelection));
